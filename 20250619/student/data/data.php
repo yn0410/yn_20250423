c1b37b556d278820ef7174e66244808516d50226
yn <jenny783113@gmail.com>
2025-06-19 09:22:48 +0800

    講PHP-data.php(老師code)

---
+++ 20250619/student/data/data.php
@@ -0,0 +1,27 @@
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    // var_dump($data);
+    echo "</pre>";
+}
+
+
+$data = [
+    [
+        'id' => 1,
+        'name' => 'amy',
+    ],
+    [
+        'id' => 2,
+        'name' => 'bob',
+    ],
+    [
+        'id' => 3,
+        'name' => 'cat',
+    ],
+];
+
+// dd($data);
+?>
--- 20250619/student/index.php
+++ 20250619/student/index.php
@@ -1,29 +1,5 @@
 <?php
-function dd($data)
-{
-    echo "<pre>";
-    print_r($data);
-    // var_dump($data);
-    echo "</pre>";
-}
-
-
-$data = [
-    [
-        'id' => 1,
-        'name' => 'amy',
-    ],
-    [
-        'id' => 2,
-        'name' => 'bob',
-    ],
-    [
-        'id' => 3,
-        'name' => 'cat',
-    ],
-];
-
-// dd($data);
+include "./data/data.php";
 ?>
 
 <!DOCTYPE html>
