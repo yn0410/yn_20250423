500001ac07a5fbb10da44b16c811acf63aad5f6b
yn <jenny783113@gmail.com>
2025-08-28 11:35:07 +0800

    註解

---
+++ 20250828/admin/hello.php
@@ -0,0 +1,20 @@
+<?php
+// 所有Domain都能用了(用192.168.211.57 || localhost開檔案(連進此程式)，都能正常使用，不會報錯了)
+header("Access-Control-Allow-Origin: *");
+
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+$data = [
+    'id' => 7,
+    'name' => '詹雅年',
+    'msg' => "生活本來就是在普通裡面找亮點✨"
+];
+// dd($data);
+
+echo json_encode($data);
